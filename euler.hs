ca08e2a797b8ace46a2d2c198713292a6ff4ed2e
Maarten Faddegon <me@maartenfaddegon.nl>
2013-03-31 17:12:17 +0200

    solution4

--- euler.hs
+++ euler.hs
@@ -9,17 +9,19 @@ fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
 solution2 = sum $ filter even $ takeWhile (< 4000000) fibs
 
--- the Sieve of Eratosthenes
+-- The sieve of Eratosthenes
 primesUpTo n = 2 : sieve [3,5..n]
     where sieve (p:xs)
-            | p*p > n   = p : xs
-            | otherwise = p : sieve (xs `minus` [p*p, p*p+2*p..])
+            | p^2 > n   = p : xs
+            | otherwise = p : sieve (xs `minus` [p^2, p^2+2*p..])
 
+-- The use of the '~' in the sieve pattern match delays the evaluation of the
+-- value until the component part is actually used. 
 primes = 2 : primes'
   where primes' = sieve [3,5..] 9 primes'
-        sieve (x:xs) q ps@ ~(p:t)
+        sieve (x:xs) q ps@ ~(p:t) 
             | x < q     = x : sieve xs q ps
-            | otherwise =     sieve (xs `minus` [q, q+2*p..]) (head t^2) t
+            | otherwise = sieve (xs `minus` [q, q+2*p..]) (head t^2) t
 
 -- Note that using primes rather than primesUpTo allows laziness
 -- to do its jobs and stop as soon as n is factored.
@@ -32,3 +34,14 @@ factorize n = factorize' n $ primes
                           ps''     -> ps''
 
 solution3 = last $ factorize 600851475143
+
+-- Yes, we could compare only the first half of the strings. But determining
+-- how long the string actually is, probably needs equally much time as just
+-- comparing a bit more.
+palindromic x = s == s'
+    where s  = show x
+          s' = reverse s
+
+solution4 = maximum [x*y | x <- [1..999], y <- [1..999], palindromic (x*y)]
+
+main = putStrLn $ show solution4
