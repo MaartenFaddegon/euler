936ef52764e42d89e868f3e6af30977a5fab6478
Maarten Faddegon <mafad@pandora.net>
2022-12-20 13:58:09 +0100

    solved 22

--- rust/solution22/src/main.rs
+++ rust/solution22/src/main.rs
@@ -1,3 +1,33 @@
+use std::fs;
+
+fn cvalue(c : char) -> u32 {
+    return (c as u32) - ('A' as u32) + 1;
+}
+
+fn svalue(s : &String) -> u32 {
+    return s.chars()
+            .fold(0, |acc, ch| acc + cvalue(ch));
+}
+
 fn main() {
-    println!("Hello, world!");
+  let contents = fs::read_to_string("names.txt")
+        .expect("Should have been able to read the names.txt file");
+
+  let mut vec: Vec<String> =
+      contents.split(',')
+      .into_iter()
+      .map(|s| s.trim().replace("\"", ""))
+      .collect::<Vec<String>>();
+
+  vec.sort();
+
+  let mut pos = 1;
+  let mut total = 0;
+  for x in vec {
+      let score = pos * svalue(&x);
+      total = total + score;
+      println!("{pos}: {x} ({score})");
+      pos = pos + 1;
+  }
+  println!("total = {total}");
 }
